745a8171671f4508d8519d5fa83067d11b862d91
RomainVelocitAI <direction@velocit-ai.fr>
2025-08-26 21:25:36 +0400

    Fix TypeScript error: Change keyof JSX.IntrinsicElements to React.ElementType

--- src/components/atoms/Container.tsx
+++ src/components/atoms/Container.tsx
@@ -2,7 +2,7 @@ import { HTMLAttributes, forwardRef } from 'react'
 import { cn } from '@/lib/utils'
 
 export interface ContainerProps extends HTMLAttributes<HTMLDivElement> {
-  as?: keyof JSX.IntrinsicElements
+  as?: React.ElementType
 }
 
 const Container = forwardRef<HTMLDivElement, ContainerProps>(
